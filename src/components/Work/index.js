3b6f5a3a76bf9e4fbaa74ceb4d151b4fd579498d
alexb <vladalexbota@gmail.com>
2022-01-10 22:07:18 +0100

    Fix grid gap

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -20,43 +20,43 @@ const Work = () => {
               to use.
             </p>
           </div>
-          <div className="h-150 flex flex-col bg-blue-200 z-10 p-10 cursor-pointer transform transition duration-500 hover:scale-110 rounded-lg shadow-lg overflow-hidden">
+          <div className="h-150 col-span-2 md:col-span-1 flex flex-col bg-blue-200 z-10 p-10 cursor-pointer transform transition duration-500 hover:scale-110 rounded-lg shadow-lg overflow-hidden">
             <p className="text-text-primary">BookingScape</p>
             <h3 className="text-3xl font-bold text-text-dark mb-3">
               Room management platform
             </h3>
-            <a className="text-center bg-blue-500 w-24 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" href="https://github.com/alexbota/bookingscape-api" target="_blank">
+            <a className="text-center bg-blue-500 w-24 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" href="https://github.com/alexbota/bookingscape-api" target="_blank" rel="noreferrer">
               Check
             </a>
             <div className="w-150 my-10">
               <img src={work_item_1} alt="" />
             </div>
           </div>
-          <div className="h-150 flex flex-col bg-purple-500 z-10 p-10 cursor-pointer transform transition duration-500 hover:scale-110 rounded-lg shadow-lg overflow-hidden">
+          <div className="h-150 col-span-2 md:col-span-1 flex flex-col bg-purple-500 z-10 p-10 cursor-pointer transform transition duration-500 hover:scale-110 rounded-lg shadow-lg overflow-hidden">
             <p className="text-text-primary">Stonks</p>
             <h3 className="text-3xl font-bold text-text-dark object-contain mb-3">
               Investment simulator
             </h3>
-            <a className="text-center bg-blue-500 w-24 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" href="https://github.com/alexbota/stonks" target="_blank">
+            <a className="text-center bg-blue-500 w-24 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" href="https://github.com/alexbota/stonks" target="_blank" rel="noreferrer">
               Check
             </a>
             <div className="w-150 my-10">
               <img src={work_item_2} alt="" />
             </div>
           </div>
-          <div className="h-150 flex flex-col bg-gray-400 z-10 p-10 cursor-pointer transform transition duration-500 hover:scale-110 rounded-lg shadow-lg overflow-hidden">
+          <div className="h-150 col-span-2 md:col-span-1 flex flex-col bg-gray-400 z-10 p-10 cursor-pointer transform transition duration-500 hover:scale-110 rounded-lg shadow-lg overflow-hidden">
             <p className="text-text-primary">Searchful</p>
             <h3 className="text-3xl font-bold text-text-dark object-contain mb-3">
               Utility Web App for Components Searching
             </h3>
-            <a className="text-center bg-gray-200 w-24 text-white font-bold py-2 px-4 rounded-full">
+            <button className="text-center bg-gray-200 w-24 text-white font-bold py-2 px-4 rounded-full">
               Soon
-            </a>
+            </button>
             <div className="w-120 mt-10">
               <img src={work_item_3} alt="" />
             </div>
           </div>
-          <div className="h-150 flex flex-col bg-blue-300 z-10 p-10 cursor-pointer transform transition duration-500 hover:scale-110 rounded-lg shadow-lg overflow-hidden">
+          <div className="h-150 col-span-2 md:col-span-1 flex flex-col bg-blue-300 z-10 p-10 cursor-pointer transform transition duration-500 hover:scale-110 rounded-lg shadow-lg overflow-hidden">
             <div className="w-120 my-10">
               <img src={work_item_4} alt="" />
             </div>
@@ -64,9 +64,9 @@ const Work = () => {
             <h3 className="text-3xl font-bold text-text-dark object-contain mb-3">
               Data Analysis Dashboard
             </h3>
-            <a className="text-center bg-gray-200 w-24 text-white font-bold py-2 px-4 rounded-full">
+            <button className="text-center bg-gray-200 w-24 text-white font-bold py-2 px-4 rounded-full">
               Soon
-            </a>
+            </button>
           </div>
         </div>
       </div>
